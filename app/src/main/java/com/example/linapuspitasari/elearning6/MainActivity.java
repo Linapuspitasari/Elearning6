ed2cc5769c2d7980f9dafda5fbad6edf7cf276f5
Lina Puspitasari <puspitalina61@gmail.com>
2018-11-09 09:23:34 +0700

    button login

--- app/src/main/java/com/example/linapuspitasari/elearning6/MainActivity.java
+++ app/src/main/java/com/example/linapuspitasari/elearning6/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.linapuspitasari.elearning6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, login.class);
+                startActivity(login);
+            }
+        });
+        }
     }
-}
